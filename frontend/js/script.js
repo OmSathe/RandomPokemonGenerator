f36b02af3a3fc728808f93d25fa4a3cb57b66912
Om Sathe <om.sathe@ontariotechu.net>
2024-08-13 03:08:32 -0400

    JS working properly
    able to generate a random pokemon from first to current generation.
    Will print the front sprite of that pokemon
    
    FOR FUTURE
    - Be able to create a team of 6 randomly generated pokemon
    - Able to generate stats and items as well
    - create a mini game where two people can generate 6 pokemon and be able to use them as cards which functions like a mini rock paper scissors...
          -> works as person 1 and 2 choose 1 out of their 6 randomly generated pokemon which will battle with each other resulting in a win, loss or tie.
    
    there will be a total of 6 rounds and the winner of the round will be determined by who has a winning type advantage...
    
    for example
    water pokemon beats fire because water to fire is 2x weakness where as fire to water is 0.5x weakness.
    
    a tie case scenario would be two dragon types as they are both 2x weakness to each other

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,31 @@
-fetch("https://pokeapi.co/api/v2/pokemon/decidueye")
-    .then(response => response.json())
-    .then(data => console.log(data.name))
-    .catch(error => console.error(error));
+//function to generate a random number
+//current amount of pokemon = 1025
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function generatePokemon(){
+
+    //chooses a random pokemon using their id number
+    randomPokemon = "https://pokeapi.co/api/v2/pokemon/" + String(getRandomInt(1,1025))
+
+    try{
+        const response = await fetch(randomPokemon)
+
+        if(!response.ok){
+            throw new Error("could not fetch resource");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite")
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }  
+    catch(error){
+        console.error(error);
+    }
+}
